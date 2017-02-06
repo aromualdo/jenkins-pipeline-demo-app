b5cd5ef95ee764f5bd4625c787b4109d4a319300
Ashley Romualdo <ashley.romualdo@mainstreethub.com>
2017-02-06 17:21:18 -0600

    Simplifying global

--- src/mainstreethub/pipelines/Dropwizard.groovy
+++ src/mainstreethub/pipelines/Dropwizard.groovy
@@ -1,10 +1,10 @@
 package mainstreethub.pipelines;
-
-class Dropwizard implements Serializable{
-  def steps
-  Dropwizard(steps) { this.steps = steps }
-
+//
+//class Dropwizard implements Serializable{
+//  def steps
+//  Dropwizard(steps) { this.steps = steps }
+//
   def notifySlack() {
     println "I'm notifying";
   }
-}
+//}
