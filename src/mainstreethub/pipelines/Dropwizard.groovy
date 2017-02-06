0dc488d1cb6b9d3298e29b25ef2a71831a7c0773
Ashley Romualdo <ashley.romualdo@mainstreethub.com>
2017-02-06 11:54:19 -0600

    Fixing up

--- src/mainstreethub/pipelines/Dropwizard.groovy
+++ src/mainstreethub/pipelines/Dropwizard.groovy
@@ -1,16 +1,13 @@
 package mainstreethub.pipelines;
 
-@Library('Dropwizard')
-class Dropwizard implements Serializable {
-    def notify() {
-        echo "I'm notifying";
-//        String message = "Hello";
-//        boolean success = true;
-//        slackSend(
-//                channel: "script-test",
-//                color: success ? "good" : "danger",
-//                message: "${message} - ${env.BUILD_URL}",
-//                tokenCredentialId: "slack-integration-token"
-//        )
-    }
+def notify() {
+  echo "I'm notifying";
+//  String message = "Hello";
+//  boolean success = true;
+//  slackSend(
+//          channel: "script-test",
+//          color: success ? "good" : "danger",
+//          message: "${message} - ${env.BUILD_URL}",
+//          tokenCredentialId: "slack-integration-token"
+//  )
 }
